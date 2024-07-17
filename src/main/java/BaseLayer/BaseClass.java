7ffebf1f1c6659fc676388448543680e0c4d3f5e
milindgadge23 <user@DESKTOP-F0HMF9A>
2024-07-17 14:49:38 +0530

    UserName TC is Created

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
+	public static WebDriver driver;
 	public static void initialization() {
 		
+		
 	
-	WebDriver driver = new ChromeDriver();
+	 driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30)).pageLoadTimeout(Duration.ofSeconds(30));
 	driver.get("https://www.saucedemo.com/");
